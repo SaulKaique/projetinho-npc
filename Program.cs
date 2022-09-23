fb9e617430f2695f4c3d16a360cc1d19fe02a9f8
saulkaique <saul.silva@etec.sp.gov.br>
2022-09-23 16:56:22 -0300

    começo do projetinho NPC pedir ajuda para o professor para saber se tem como trasformar uma frase em uma variante

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+
+Console.Write("Bom Dia Meu nome e yosano você deve ser novo a qui qual Seu nome? \n "  );
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.ResetColor();
+
+
+Console.Write("Digite seu nome:");
+string Nome =  Console.ReadLine()!;
+
+Console.WriteLine($"Então Se Nome e {Nome} Que Nome legal {Nome} Esta tudo bem Com você \n ");
+
+string SimouNao = Console.ReadLine()!;
+
+
+
+Console.WriteLine($"{SimouNao} que LEGAL você parece ser alguem muito legal que ser meu amigo? \n ");
+SimouNao = Console.ReadLine()!;
