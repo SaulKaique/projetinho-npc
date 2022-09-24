58cc787bdd96549855b733c181795f3cd1643861
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-23 22:16:20 -0300

    [23/09/2022] Update de Projeto NPC

--- Program.cs
+++ Program.cs
@@ -1,20 +1,112 @@
-﻿Console.Clear();
-
-
-Console.Write("Bom Dia Meu nome e yosano você deve ser novo a qui qual Seu nome? \n "  );
-
+﻿string npcNOME = "Yosano";
+Console.Clear();
+string frase = $"Bom Dia\nMeu nome é {npcNOME} você deve ser novo aqui\nQual seu nome?\n";
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
 Console.ForegroundColor = ConsoleColor.DarkYellow;
+frase = "Digite seu nome: ";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+Console.ForegroundColor = ConsoleColor.DarkRed;
+string Nome =  Console.ReadLine()!;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+frase = $"Então Seu Nome é {Nome}. \nQue nome legal. \n{Nome} esta tudo bem Com você?\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+frase = "Fale como você esta com Y (yes) ou N (no) \n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+// YES OU NO (RESPOSTA) ----------------------------
+
+string resposta = Console.ReadLine()!;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+int valor = new Random().Next(1,21);
+int valor2 = new Random().Next(1,21);
+if (resposta.Contains("N") == true) 
+{
+frase = $"Não ?? que triste se eu podese te daria um abraço voce quer comversar falam que isso ajuda nessas horas.\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+}
+else if (resposta.Contains("Y") == true)
+{
+    frase = $"Então vamos brincar, se eu tirar dado maior que o seu você perde :)\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+// VALORES DOS DADOS ----------------------------
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    frase = $"{npcNOME} tirou {valor} nos dados.\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+} 
+    frase = $"{Nome} tirou {valor2} nos dados.\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+} 
+
+// FRASES DE VITORIAS - PERDAS - EMPATES ----------------------------
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+if (valor > valor2)
+{
+    frase = $"{Nome} você perdeu >:)\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+}
+else if (valor < valor2)
+{
+    frase = $"{Nome} você ganhou!\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+}
+else if (valor == valor2)
+{
+    frase = $"Empate :|\n";
+for (int i = 0; i < frase.Length; i++)
+{
+Console.Write(frase[i]);   
+Thread.Sleep(75); 
+}
+}
+}
 Console.ResetColor();
 
 
-Console.Write("Digite seu nome:");
-string Nome =  Console.ReadLine()!;
-
-Console.WriteLine($"Então Se Nome e {Nome} Que Nome legal {Nome} Esta tudo bem Com você \n ");
-
-string SimouNao = Console.ReadLine()!;
 
 
 
-Console.WriteLine($"{SimouNao} que LEGAL você parece ser alguem muito legal que ser meu amigo? \n ");
-SimouNao = Console.ReadLine()!;
+
+
+
